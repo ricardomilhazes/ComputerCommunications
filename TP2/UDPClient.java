676aa12d3d5e648bdc60835c3dd2b8829f9f0293
ricardomilhazes <rmilhazesveloso@gmail.com>
2019-04-18 01:06:34 +0100

    UDPClient

--- TP2/UDPClient.java
+++ TP2/UDPClient.java
@@ -1,24 +1,37 @@
 import java.io.*;
 import java.net.*;
 
-class UDPClient
-{
-   public static void main(String args[]) throws Exception
-   {
-      BufferedReader inFromUser =
-         new BufferedReader(new InputStreamReader(System.in));
-      DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("localhost");
-      byte[] sendData = new byte[1024];
-      byte[] receiveData = new byte[1024];
-      String sentence = inFromUser.readLine();
-      sendData = sentence.getBytes();
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-      clientSocket.send(sendPacket);
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      clientSocket.receive(receivePacket);
-      String modifiedSentence = new String(receivePacket.getData());
-      System.out.println("FROM SERVER:" + modifiedSentence);
-      clientSocket.close();
-   }
+class UDPClient{
+	Transferecc cc;
+	DatagramSocket clientSocket;
+    byte[] sendData = new byte[1024];
+    byte[] receiveData = new byte[1024];
+
+    public UDPClient() throws Exception{
+    	cc = new Transferecc();
+    	clientSocket = new DatagramSocket();
+    }
+
+    public void send(String sentence, InetAddress IPAddress) throws Exception{
+  
+	    sendData = sentence.getBytes();
+	    IPAddress = InetAddress.getByName("localhost");
+	    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+	    clientSocket.send(sendPacket);
+    }
+
+   public void run(){
+   		try{
+   			while(true){
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			    clientSocket.receive(receivePacket);
+			    String modifiedSentence = new String(receivePacket.getData());
+			    System.out.println("FROM SERVER:" + modifiedSentence);
+			}
+   		} catch(Exception e) {
+   		e.printStackTrace();
+   		} finally {
+   		clientSocket.close();
+   		}
+   	}
 }
